5ecef6eef3cd956f0b4c488a397133d327161833
sasasan03 <sako.hiro62@gmail.com>
2023-07-07 23:06:26 +0900

    first commit

---
+++ StepWatchApp/StepWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepWatchAppApp.swift
+//  StepWatchApp
+//
+//  Created by sako0602 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct StepWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
